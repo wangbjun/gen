9d5d8b2f7b4e87d0c03a5598fdcff5a6b264e9f9
wangbenjun <wangbenjun@gmail.com>
2019-11-19 19:19:58 +0800

    路由优化

--- router/Router.go
+++ router/Router.go
@@ -11,24 +11,25 @@ func Route(Router *gin.Engine) {
 
 	Router.GET("/", BaseController.Index)
 
-	Router.Group("/api")
+	api := Router.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		v1.Group("/articles").
+			GET("/", ArticleController.ListArticle).  //文章列表
+			GET("/:id", ArticleController.GetArticle) //文章详情
 
-	Router.Group("/api/v1/articles").
-		GET("/", ArticleController.ListArticle).  //文章列表
-		GET("/:id", ArticleController.GetArticle) //文章详情
+		v1.Group("/articles").Use(middleware.Auth()).
+			POST("/", ArticleController.AddArticle).             //添加文章
+			POST("/:id", ArticleController.EditArticle).         //修改文章
+			DELETE("/:id", ArticleController.DelArticle).        //删除文章
+			POST("/:id/comments", ArticleController.AddComment). //添加评论
+			GET("/:id/comments", ArticleController.ListComment)  //评论列表
 
-	Router.Group("/api/v1/articles").Use(middleware.Auth()).
-		POST("/", ArticleController.AddArticle).             //添加文章
-		POST("/:id", ArticleController.EditArticle).         //修改文章
-		DELETE("/:id", ArticleController.DelArticle).        //删除文章
-		POST("/:id/comments", ArticleController.AddComment). //添加评论
-		GET("/:id/comments", ArticleController.ListComment)  //评论列表
+		v1.Group("/user").
+			POST("/register", UserController.Register). //用户注册
+			POST("/login", UserController.Login)        //用户登录
 
-	Router.Group("/api/v1/user").
-		POST("/register", UserController.Register). //用户注册
-		POST("/login", UserController.Login)        //用户登录
-
-	Router.Group("/api/v1/user").Use(middleware.Auth()).
-		POST("/logout", UserController.Logout) //用户退出登录
-
+		v1.Group("/user").Use(middleware.Auth()).
+			POST("/logout", UserController.Logout) //用户退出登录
+	}
 }
